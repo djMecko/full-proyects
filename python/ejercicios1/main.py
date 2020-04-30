77e7ed6aa9d19ac35d5866c7bd33b4991c209a32
Max Zeballos <max.zeballos@rankmi.com>
2020-04-29 23:21:44 -0500

    add homework

---
+++ python/ejercicios1/main.py
@@ -0,0 +1,16 @@
+import math
+
+# Ejercicios 1: Escribir un ejemplo con cada funcion de la libreria math y entenderla
+# Si tiene conceptos matematicos desconocidos, solo hacer el ejemplo y pasar al siguiente
+# Documentation: https://docs.python.org/3/library/math.html
+
+print(math.ceil(10.11))	# Rpta: 11
+# print(math.comb(10, 4)) # Error not working
+print(math.fabs(-7.7))	#Rpta: 7.7
+
+
+# Ejercicios 2: Escribir un ejemplo para cada funcion de string
+# Documentation: https://docs.python.org/2.5/lib/string-methods.html
+texto_prueba = "RoDriGo"
+print(texto_prueba.capitalize())
+print(texto_prueba.center(20, '-'))
