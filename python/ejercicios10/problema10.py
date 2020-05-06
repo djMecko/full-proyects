26fd96682135a8fb86419e9f369f7de2c0a736b1
rodrigo zeballos torres <rodrigopzt900@hotmail.com>
2020-05-05 22:01:48 -0500

    Problema 10

---
+++ python/ejercicios10/problema10.py
@@ -0,0 +1,33 @@
+array_nombre=[]
+array_edad=[]
+def menu():
+    print("Seleccione un opciòn del 1 al 3")
+    print("1: Agregar estudiante")
+    print("2: Ver estudiantes")
+    print("3: Salir del programa")
+
+    seleccion=int(input())
+    
+
+    if seleccion == 1:
+        print("Ingresa el nombre")
+        name=str(input())
+        print("Ingresa la edad")
+        age=str(input())
+        array_nombre.append(name)
+        array_edad.append(age)
+        return menu()
+
+    elif seleccion == 2:
+        print("Imprimiendo Lista de estudiantes")
+        for i in range(len(array_nombre)):
+            print(i+1,"Nombre: ",array_nombre[i]," Edad: ", array_edad[i],"\n")
+    
+    elif seleccion == 3:
+        print("Usted esta saliendo del programa...adios")
+    
+    else:
+        print("No seas mamon , te dijimos del uno al 3")
+    
+        
+menu()    
