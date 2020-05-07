53fe5210470d47e39f192f238956a42fc232bf02
rodrigo zeballos torres <rodrigopzt900@hotmail.com>
2020-05-06 23:58:43 -0500

    Diccionario

---
+++ python/ejercicios12/diccionario.py
@@ -0,0 +1,54 @@
+class Dictionary:
+    palabras = []
+    significados = []
+    def __init__(self,palabra,significado):
+        self.palabra = palabra
+        self.significado = significado
+    
+    def add_word(self):
+        Dictionary.palabras.append(self.palabra)
+        Dictionary.significados.append(self.significado)
+    def search_word(self):
+        print(Dictionary.palabras)
+        print(Dictionary.significados)
+
+        
+def menu():
+    print("Que desea hacer: ")
+    print("1:Añadir palabras: ")
+    print("2:Ver palabras: ")
+    print("3:Buscar palabras: ")
+    dictionary = Dictionary("","") 
+
+    seleccion = int(input())
+    if seleccion == 1:
+        print("Añadir palabra")
+        a= str(input())
+        print("Añadir significado")
+        b= str(input())
+        dictionary = Dictionary(a,b) 
+        
+        print("Su palabra ha sido agregada exitosamente")
+        dictionary.add_word()
+        return menu()
+        
+    elif seleccion == 2:
+        print("Disponible Proximamente")
+        dictionary.search_word()
+        print("Busque su palabra")
+        word = str(input())
+        for i in range(0,len(dictionary.palabras)):
+            if dictionary.palabras[i] == word:
+                print("Palabra: "+ dictionary.palabras[i] + " ,Significado: " + dictionary.significados[i])
+            elif dictionary.palabras[i] != word:
+                print("Palabras no encontrada")
+        return menu()
+    else:
+        print("Ingrese un valor correcto por favor")
+        return menu()
+
+    
+menu()
+
+
+
