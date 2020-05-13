a5fee2fb09e12e2ef3791b8796570332bfb3b407
rodrigo zeballos torres <rodrigopzt900@hotmail.com>
2020-05-13 01:58:50 -0500

    DICCIONARIO

--- python/ejercicios15/main2.py
+++ python/ejercicios15/main2.py
@@ -69,9 +69,82 @@ def ejercicio8():
             print(directorio[i],end="")
         else:
             print(i)
-    
-    
-    
+
+def ejercicio9():
+    pagado=0
+    deuda=0 
+    numero="P"
+    directorio={}
+    while numero!="T":
+        numero=input("¿Quieres añadir una nueva factura (A), pagarla (P) o terminar (T)?")
+        if numero=="A":
+            print("Usted eligio nueva factura")
+            num=input("Ingrese numero de orden: ")
+            factura=input(num+": ")
+            directorio[num]=factura
+            #for i,j in directorio.items():
+            #    print(i,j)
+            #    deuda+=float(j)
+            deuda=int(deuda)+int(factura)
+            print("La suma de las deudas es: ",deuda)
+            print("Lo que ha pagado",pagado)
+        elif numero=="P":
+            print(directorio)
+            orden=str(input("Ingrese el numero de factura que desea pagar"))
+            tmp=0
+            
+            for i,j in directorio.items():
+                if i==orden:
+                    tmp=j
+            directorio.pop(orden)
+            print(directorio)
+            pagado=pagado+int(tmp)
+            deuda=deuda-int(tmp)
+            print("La suma de las deudas es: ",deuda)
+            print("Lo que ha pagado",pagado)
+        elif numero=="T":
+            print("T")
+        else:
+            print("Introduzca una letra valida")
+        
+def ejercicio10():
+    clients = {}
+    option = ''
+    while option != '6':
+        if option == '1':
+            nif = input('Introduce NIF del cliente: ')
+            name = input('Introduce el nombre del cliente: ')
+            address = input('Introduce la dirección del cliente: ')
+            phone = input('Introduce el teléfono del cliente: ')
+            email = input('Introduce el correo electrónico del cliente: ')
+            vip = input('¿Es un cliente preferente (S/N)? ')
+            client = {'nombre':name, 'dirección':address, 'teléfono':phone, 'email':email, 'preferente':vip=='S'}
+            clients[nif] = client
+        if option == '2':
+            nif = input('Introduce NIF del cliente: ')
+            if nif in clients:
+                del clients[nif]
+            else:
+                print('No existe el cliente con el nif', nif)
+        if option == '3':
+            nif = input('Introduce NIF del cliente: ')
+            if nif in clients:
+                print('NIF:', nif)
+                for key, value in clients[nif].items():
+                    print(key.title() + ':', value)
+            else:
+                print('No existe el cliente con el nif', nif)
+        if option == '4':
+            print('Lista de clientes')
+            for key, value in clients.items():
+                print(key, value['nombre'])
+        if option == '5':
+            print('Lista de clientes preferentes')
+            for key, value in clients.items():
+                if value['preferente']:
+                    print(key, value['nombre'])
+        option = input('Menú de opciones\n(1) Añadir cliente\n(2) Eliminar cliente\n(3) Mostrar cliente\n(4) Listar clientes\n(5) Listar clientes preferentes\n(6) Terminar\nElige una opción:')
+
 
 #ejercicio1("Nuevo sol peruano")
 #ejercicio1("Yen")
@@ -82,4 +155,6 @@ def ejercicio8():
 #ejercicio5()
 #ejercicio6()
 #ejercicio7()
-ejercicio8()
+#ejercicio8()
+#ejercicio9()
+ejercicio10()
